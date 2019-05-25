3b1929752e55ecd4c6422d53f5c1612cd1252766
Meepaco <lemeep123@gmail.com>
2019-05-25 12:16:58 -0400

    Migrated message text to json, update help text

---
+++ README.md
@@ -0,0 +1,17 @@
+A sketchy discord bot by Meepco
+
+
+
+Features:
+
+-Spam
+-Ping
+-Commands to paste stuff
+
+
+
+Issues:
+
+-Spam always spams one less message
+-Specifying what to spam is dead
+
--- YeetMePls.js
+++ YeetMePls.js
@@ -4,9 +4,7 @@ var config = require('./Renograde.json');
 var prefix = config.bot.prefix;
 
 
-
-
-
+//loading
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
     //servers
@@ -20,7 +18,7 @@ client.on('ready', () => {
            })
     var generalChannel = client.channels.get("500665868318015489") // channel ID
         generalChannel.send("I is the online")    
-        console.log("finished loading")         
+        console.log("-----finished loading-----")         
          })
        })
 
@@ -30,28 +28,30 @@ client.on('message', async receivedMessage => {
         return
         }
 
-    //prefix = ("-")
-
-var essay1 = "shut yo skin tone chicken bone google chrome no home flip phone disowned ice cream cone garden gnome extra chromosome metronome dimmadome genome full blown monochrome student loan indiana jones overgrown flintstone x and y hormone friend zone sylvester stallone sierra leone autozone professionally seen silver patrone ching chong ling long suck my ding dong head ass pubg fortnite flip phone remote control autism down syndrome stage four terminal braind cancer o rielly auto parts silver bronze ash amiibo uv light pen sushi ramen harrison ford gamer bitch ass virgin lamp thermometor lean mean string bean charlie sheen limousine canteen trampoline serpentine antihistamine wolverine submarine unclean nectarine broken gene halloween defective spleen smokescreen james dean putting green tiny peen anti vaccine aquamarine eugene extra green nicotine vaseline jellybean magazine protein lightning-mcqueen vending machine what'chu mean Ocean Man by Ween head ass tf up bitch"
-var essay2 = `Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah Bwah bwah bwah bwah bwah`
+//Commands
+var essay = config.bot.essay  
+var bwah = config.bot.bwah   
 var helptext = `**Commands:** 
 
 **spam <amount> ~~<thing>~~:** Spams a ~~user defined~~ message (x) number to times
-
 **ping:** Gets latency.
+**help:** Shows this message.
 
-**help:** Shows this message.` 
+***----Message Commands----***
+
+**essay** Best roast
+**bwah** ehhhh....` 
 
 
-    if (receivedMessage == prefix + ("essay1")) {
-        receivedMessage.channel.send(essay1)
+    if (receivedMessage == prefix + ("essay")) {
+        receivedMessage.channel.send(essay)
         console.log("Author: " + receivedMessage.author + "**Command**: " + receivedMessage)
-        console.log("**Output**: " + essay1)
+        console.log("**Output**: " + essay)
     }
-    if (receivedMessage == prefix + ("essay2")) {
-        receivedMessage.channel.send(essay2)
+    if (receivedMessage == prefix + ("bwah")) {
+        receivedMessage.channel.send(bwah)
         console.log("Author: " + receivedMessage.author + "**Command**: " + receivedMessage)
-        console.log("**Output**: " + essay2)
+        console.log("**Output**: " + bwah)
   }
     if (receivedMessage == prefix + "ping") {
     const m = await receivedMessage.channel.send("Ping?");
@@ -96,6 +96,6 @@ else  {
   }
   
 })
-//renograde = "" //bot token
+
 client.login(config.bot.token);
 
